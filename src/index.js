1adec5842ffed53b6c73141f714a4dffc81b8461
MaryTreasure <mari199191@mail.ru>
2024-07-26 18:24:32 +0300

    feat: implement reverse int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+    let resultArr = [];
+    let strFromNumber =`${n > 0? n.toString(): (n * -1).toString()}`
   
+    for(let i = strFromNumber.length - 1; i >= 0; i--) {
+      resultArr.push(strFromNumber[i]);
+      console.log(strFromNumber[i]);
+    }
+     return +resultArr.join('');
 }
